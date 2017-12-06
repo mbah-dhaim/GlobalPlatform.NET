237d09a118a27501bedcc6cff07ac99b07e69b31
James Harling <james@jamesharling.me>
2017-12-06 19:15:55 +0000

    Add XML documentation

--- src/GlobalPlatform.NET/Reference/ApduClass.cs
+++ src/GlobalPlatform.NET/Reference/ApduClass.cs
@@ -2,8 +2,19 @@
 {
     public static class ApduClass
     {
+        /// <summary>
+        /// Command defined in ISO/IEC 7816.
+        /// </summary>
         public static byte Iso7816 = 0x00;
+
+        /// <summary>
+        /// GlobalPlatform command.
+        /// </summary>
         public static byte GlobalPlatform = 0x80;
+
+        /// <summary>
+        /// Secure messaging – GlobalPlatform proprietary.
+        /// </summary>
         public static byte SecureMessaging = 0x84;
     }
 }
